3ed59fe114705a0059f8ac35f0cce0ad28b2ba72
CHAT-DISPARU <gaetanfranca@proton.me>
2026-01-17 18:47:19 +0100

    add main

---
+++ includes/minirt.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gajanvie <gajanvie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/17 18:42:01 by gajanvie          #+#    #+#             */
+/*   Updated: 2026/01/17 18:46:36 by gajanvie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "libft.h"
+# include "gajanvielib_math.h"
+# include <stdio.h>
+# include <math.h>
+
+
+
+#endif
