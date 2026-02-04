4500264d13d6b9a60a7d0a934b9c633cd9ac017f
CHAT-DISPARU <gajanvie@3D2.42angouleme.fr>
2026-02-04 13:27:41 +0100

    ajout cone mais pas sur des math pour le scaling a revoir

---
+++ srcs_bonus/hit_objs/hit_co_bonus.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hit_co_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gajanvie <gajanvie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 12:17:23 by gajanvie          #+#    #+#             */
+/*   Updated: 2026/02/04 13:27:15 by gajanvie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minirt_bonus.h>
+
+bool	hit_border_co(t_ray ray, double x_plane, double *t_out, double rad)
+{
+	double	t;
+	double	y;
+	double	z;
+
+	if (fabs(ray.dir.x) < EPSILON)
+		return (false);
+	t = (x_plane - ray.origin.x) / ray.dir.x;
+	if (t < 0.001)
+		return (false);
+	y = ray.origin.y + t * ray.dir.y;
+	z = ray.origin.z + t * ray.dir.z;
+	if ((y * y + z * z) > rad)
+		return (false);
+	*t_out = t;
+	return (true);
+}
+
+void	see_cap(t_hit hit, t_cy_utils *utils, double height, double rad)
+{
+	if (hit_border_co(hit.l_ray, height, &hit.t, rad))
+	{
+		if (hit.t < utils->closest_t)
+		{
+			utils->hit_zone = 2;
+			utils->closest_t = hit.t;
+		}
+	}
+}
+
+void	see_poly_co(t_hit hit, t_vec3 poly, double delta, t_cy_utils *utils)
+{
+	double		hit_x;
+
+	hit.t = (-poly.y - sqrt(delta)) / (poly.x);
+	if (hit.t > EPSILON)
+	{
+		hit_x = hit.l_ray.origin.x + hit.t * hit.l_ray.dir.x;
+		if (hit_x >= 0 && hit_x <= utils->half_h)
+		{
+			utils->closest_t = hit.t;
+			utils->hit_zone = 1;
+		}
+	}
+	if (utils->hit_zone == 0)
+	{
+		hit.t = (-poly.y + sqrt(delta)) / poly.x;
+		if (hit.t > EPSILON)
+		{
+			hit_x = hit.l_ray.origin.x + hit.t * hit.l_ray.dir.x;
+			if (hit_x >= 0 && hit_x <= utils->half_h)
+			{
+				utils->closest_t = hit.t;
+				utils->hit_zone = 1;
+			}
+		}
+	}
+}
+
+t_hit_r	set_rec_co(t_ray ray, t_cy_utils utils, t_obj *cy, t_hit hit, double k)
+{
+	t_hit_r	rec;
+
+	rec.t = utils.closest_t;
+	rec.p = vec_add(ray.origin, vec_scale(ray.dir, utils.closest_t));
+	if (utils.hit_zone == 1)
+	{
+		hit.local_normal = vec_add(hit.l_ray.origin,
+				vec_scale(hit.l_ray.dir, utils.closest_t));
+		hit.local_normal.x = -k * hit.local_normal.x;
+	}
+	else if (utils.hit_zone == 2)
+		hit.local_normal = (t_vec3){1, 0, 0};
+	rec.normal = mat4_mult_vec3(&cy->transform, hit.local_normal, 0.0);
+	rec.normal = vec_normalize(rec.normal);
+	if (vec_dot_scal(ray.dir, rec.normal) > 0)
+		rec.normal = vec_scale(rec.normal, -1.0);
+	return (rec);
+}
+
+bool	hit_cone(t_obj *co, t_ray ray, t_hit_r *rec)
+{
+	t_vec3		poly;
+	double		delta;
+	t_hit		hit;
+	t_cy_utils	utils;
+	double		k = pow(co->rad_1, 2) / pow(co->height, 2);
+
+	utils.closest_t = INFINITY;
+	utils.hit_zone = 0;
+	utils.half_h = co->height;
+	hit.l_ray.origin = mat4_mult_vec3(&co->inverse_transform, ray.origin, 1.0);
+	hit.l_ray.dir = mat4_mult_vec3(&co->inverse_transform, ray.dir, 0.0);
+	poly.x = -k * (hit.l_ray.dir.x * hit.l_ray.dir.x) + (hit.l_ray.dir.y * hit.l_ray.dir.y)
+		+ (hit.l_ray.dir.z * hit.l_ray.dir.z);
+	poly.y = -k * (hit.l_ray.origin.x * hit.l_ray.dir.x)+ ((hit.l_ray.origin.y * hit.l_ray.dir.y)
+			+ (hit.l_ray.origin.z * hit.l_ray.dir.z));
+	poly.z = -k * (hit.l_ray.origin.x * hit.l_ray.origin.x) + (hit.l_ray.origin.y * hit.l_ray.origin.y)
+		+ (hit.l_ray.origin.z * hit.l_ray.origin.z);
+	delta = (poly.y * poly.y) - (poly.x * poly.z);
+	if (delta >= 0)
+		see_poly_co(hit, poly, delta, &utils);
+	see_cap(hit, &utils, utils.half_h, pow(co->rad_1, 2));
+	if (utils.hit_zone == 0)
+		return (false);
+	*rec = set_rec_co(ray, utils, co, hit, k);
+	return (true);
+}
